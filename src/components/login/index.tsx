9f1886bc50f1c0f3cb763b9734cb98edfefaa66d
bskim <bskim@owlsystems.co.kr>
2024-11-13 16:07:31 +0900

    chore: 구문 string edit

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -8,7 +8,7 @@ const Login = () => {
     <div className="screen flex-center flex-col">
       <div className="flex-col">
         <h1>상상하는 무엇이든 만들 수 있어요</h1>
-        <h1 className="text-gray-400">계정에 로그인</h1>
+        <h1 className="text-gray-400">Untitled_01 계정에 로그인</h1>
         <Separator className="my-6" />
         <LoginForm />
       </div>
