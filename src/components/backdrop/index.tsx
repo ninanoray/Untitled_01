e1ab8e437c3814067166e1d194de77ecb6ad5b01
bskim <bskim@owlsystems.co.kr>
2024-11-13 14:48:15 +0900

    feat: 로그인 페이지 생성

--- src/components/backdrop/index.tsx
+++ src/components/backdrop/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 import { SidebarInset, SidebarProvider } from "../ui/sidebar";
 import { AppSidebar } from "./sidebar/app-sidebar";
@@ -8,12 +9,15 @@ interface Props {
   children: ReactNode;
 }
 const Backdrop = ({ children }: Props) => {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>{children}</SidebarInset>
-    </SidebarProvider>
-  );
+  const currentPath = usePathname();
+  if (!currentPath.includes("/login"))
+    return (
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>{children}</SidebarInset>
+      </SidebarProvider>
+    );
+  else return <>{children}</>;
 };
 
 export default Backdrop;
