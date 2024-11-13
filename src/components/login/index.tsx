77b6be83527ca3d9c54a3e499be71b87275abcf2
bskim <bskim@owlsystems.co.kr>
2024-11-13 15:50:19 +0900

    feat: login form 구현
    이메일을 입력하고 submit 하면 메인으로 이동

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,6 +1,7 @@
-import { Input } from "../ui/input";
-import { Label } from "../ui/label";
+"use client";
+
 import { Separator } from "../ui/separator";
+import LoginForm from "./loginForm";
 
 const Login = () => {
   return (
@@ -9,14 +10,7 @@ const Login = () => {
         <h1>상상하는 무엇이든 만들 수 있어요</h1>
         <h1 className="text-gray-400">계정에 로그인</h1>
         <Separator className="my-6" />
-        <div className="space-y-1">
-          <Label htmlFor="email">이메일</Label>
-          <Input
-            id="email"
-            type="email"
-            placeholder="이메일 주소를 입력해주세요."
-          />
-        </div>
+        <LoginForm />
       </div>
     </div>
   );
---
+++ src/components/login/loginForm.tsx
@@ -0,0 +1,60 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { loginFormSchema } from "./loginFormSchema";
+
+const LoginForm = () => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof loginFormSchema>) {
+    console.log(values);
+    router.push("/");
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>이메일</FormLabel>
+              <FormControl>
+                <Input placeholder="이메일 주소를 입력하세요" {...field} />
+              </FormControl>
+              <FormDescription>
+                팀원과 쉽게 협업하려면 조직 이메일을 사용하세요.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          계속
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default LoginForm;
