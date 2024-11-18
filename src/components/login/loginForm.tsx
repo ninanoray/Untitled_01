c16fc6a9e5f466a87647db0d3ccc095b5e2fac3b
bskim <bskim@owlsystems.co.kr>
2024-11-18 10:41:35 +0900

    feat: 비밀번호 입력창 구현

--- src/components/login/loginForm.tsx
+++ src/components/login/loginForm.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/src/components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import {
@@ -13,47 +14,109 @@ import {
   FormMessage,
 } from "../ui/form";
 import { Input } from "../ui/input";
-import { loginFormSchema } from "./loginFormSchema";
+import { emailFormSchema, loginFormSchema } from "./loginFormSchema";
+
+const CHECK_EMAIL = 0;
+const CHECK_PASSWORD = 1;
+const CHECK_PIN = 2;
 
 const LoginForm = () => {
+  const [loginMode, setLoginMode] = useState<number>(CHECK_EMAIL);
+
   const router = useRouter();
 
-  const form = useForm<z.infer<typeof loginFormSchema>>({
-    resolver: zodResolver(loginFormSchema),
+  const formEmail = useForm<z.infer<typeof emailFormSchema>>({
+    resolver: zodResolver(emailFormSchema),
     defaultValues: {
       email: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof loginFormSchema>) {
+  const formLogin = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      password: "",
+    },
+  });
+
+  function onSubmitEmail(values: z.infer<typeof emailFormSchema>) {
+    console.log(values);
+    if (values.email.includes("test")) setLoginMode(CHECK_PASSWORD);
+    else setLoginMode(CHECK_PIN);
+  }
+
+  function onSubmitLogin(values: z.infer<typeof loginFormSchema>) {
     console.log(values);
     router.push("/");
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>이메일</FormLabel>
-              <FormControl>
-                <Input placeholder="이메일 주소를 입력하세요" {...field} />
-              </FormControl>
-              <FormDescription>
-                팀원과 쉽게 협업하려면 조직 이메일을 사용하세요.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
+    <>
+      <Form {...formEmail}>
+        <form
+          onSubmit={formEmail.handleSubmit(onSubmitEmail)}
+          className="space-y-6"
+        >
+          <FormField
+            control={formEmail.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>이메일</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="이메일 주소를 입력하세요"
+                    onInput={() => {
+                      setLoginMode(CHECK_EMAIL);
+                      formLogin.resetField("password");
+                    }}
+                    {...field}
+                  />
+                </FormControl>
+                {!formEmail.formState.errors.email &&
+                  loginMode === CHECK_EMAIL && (
+                    <FormDescription>
+                      팀원과 쉽게 협업하려면 조직 이메일을 사용하세요.
+                    </FormDescription>
+                  )}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {loginMode === CHECK_EMAIL && (
+            <Button type="submit" className="w-full">
+              계속
+            </Button>
           )}
-        />
-        <Button type="submit" className="w-full">
-          계속
-        </Button>
-      </form>
-    </Form>
+        </form>
+      </Form>
+      {loginMode === CHECK_PASSWORD && (
+        <Form {...formLogin}>
+          <form
+            onSubmit={formLogin.handleSubmit(onSubmitLogin)}
+            className="space-y-6 mt-4"
+          >
+            <FormField
+              control={formLogin.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>비밀번호</FormLabel>
+                  <FormControl>
+                    <Input placeholder="비밀번호를 입력하세요" {...field} />
+                  </FormControl>
+                  <FormDescription>비밀번호를 잊어버리셨나요?</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              비밀번호로 계속
+            </Button>
+          </form>
+        </Form>
+      )}
+    </>
   );
 };
 
--- src/components/login/loginFormSchema.ts
+++ src/components/login/loginFormSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const loginFormSchema = z.object({
+export const emailFormSchema = z.object({
   email: z
     .string()
     .min(1, { message: "이메일을 입력해주세요." })
@@ -9,4 +9,12 @@ export const loginFormSchema = z.object({
   //     const emails = await fetchEmails();
   //     return emails.includes(e);
   //   }, "This email is not in our database"),
+});
+
+export const loginFormSchema = z.object({
+  // email: z
+  //   .string()
+  //   .min(1, { message: "이메일을 입력해주세요." })
+  //   .email("유효하지 않은 이메일입니다."),
+  password: z.string().min(1, { message: "비밀번호를 입력해주세요." }),
 });
