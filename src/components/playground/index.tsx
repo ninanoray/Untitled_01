0d08174ed6aee6f3245b409b5c23147a07787bc7
bskim <bskim@owlsystems.co.kr>
2024-11-19 13:39:07 +0900

    build: markdown 라이브러리 교체
    react-markdown -> mdxeditor

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,11 +1,19 @@
+"use client";
+
+import { useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { Titlearea } from "../ui/titlearea";
 
 const Playground = () => {
+  const [content, setContent] = useState<string>("");
   return (
     <div className="size-full p-2 space-y-4">
       <Titlearea placeholder="새 페이지" />
-      <Textarea placeholder="글을 작성하거나 명령어를 사용하려면 '/' 키를 누르세요" />
+      <Textarea
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="글을 작성하거나 명령어를 사용하려면 '/' 키를 누르세요"
+      />
     </div>
   );
 };
