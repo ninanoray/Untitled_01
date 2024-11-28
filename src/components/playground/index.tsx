19dc2c906a627a15931922adb2b0702bf318d43a
bskim <bskim@owlsystems.co.kr>
2024-11-28 17:53:41 +0900

    test: ContentEditable, 다형성 컴포넌트 테스트

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,65 +1,46 @@
 "use client";
 
 import { marked } from "marked";
-import Image from "next/image";
-import { useState } from "react";
-import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import remarkGfm from "remark-gfm";
+import { useCallback, useRef, useState } from "react";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
 import { Textarea } from "../ui/textarea";
 import { Titlearea } from "../ui/titlearea";
+import Mutable from "./mutableButton";
 
 const Playground = () => {
   const [content, setContent] = useState<string>("");
   const result = marked(content);
+  const contentEditableRef = useRef<HTMLElement>(null);
+
+  const changeBlockContent = useCallback(
+    (value: string) => {
+      const cursor = document.getSelection();
+      const offset = cursor?.anchorOffset;
+      if (!(content === "" && offset === 0 && value === "")) setContent(value);
+    },
+    [content]
+  );
+
+  const onChangeContents = useCallback(
+    (event: ContentEditableEvent) => {
+      const value = event.target.value;
+      changeBlockContent(value);
+    },
+    [content, contentEditableRef]
+  );
+
   return (
     <div className="size-full p-2 space-y-4">
       <Titlearea placeholder="새 페이지" />
       {result}
       {/* <div dangerouslySetInnerHTML={{ __html: result }}></div> */}
-      <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
-        components={{
-          code({ className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || "");
-            return match ? (
-              <SyntaxHighlighter {...props} language={match[1]} PreTag="div">
-                {String(children).replace(/\n$/, "")}
-              </SyntaxHighlighter>
-            ) : (
-              <code
-                {...props}
-                className={`${className} px-3 py-1 rounded-md text-red-600 bg-stone-100`}
-              >
-                {children}
-              </code>
-            );
-          },
-          img: (image) => (
-            <Image
-              src={image.src || ""}
-              alt={image.alt || ""}
-              width={500}
-              height={350}
-            />
-          ),
-          ul({ className, ...props }) {
-            return (
-              <ul {...props} className={`${className} list-disc list-inside`} />
-            );
-          },
-          ol({ className, ...props }) {
-            return (
-              <ol
-                {...props}
-                className={`${className} list-decimal list-inside`}
-              />
-            );
-          },
-        }}
-      >
-        {content}
-      </ReactMarkdown>
+      <ContentEditable
+        placeholder={"글을 작성하거나 명령어를 사용하려면 '/' 키를 누르세요"}
+        html={result as string}
+        innerRef={contentEditableRef}
+        onChange={onChangeContents}
+      />
+      <Mutable as="button">{"안"}</Mutable>
       <Textarea
         value={content}
         onChange={(e) => setContent(e.target.value)}
---
+++ src/components/playground/mutableButton.tsx
@@ -0,0 +1,19 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+
+type Props = undefined;
+
+type MutableElementProps<T extends ElementType> = {
+  as?: T;
+  children: ReactNode;
+} & Omit<ComponentPropsWithoutRef<T>, keyof Props>;
+
+const Mutable = <T extends ElementType = "div">({
+  as,
+  children,
+  ...props
+}: MutableElementProps<T>) => {
+  const Component = as || "div";
+  return <Component {...props}>{children}</Component>;
+};
+
+export default Mutable;
